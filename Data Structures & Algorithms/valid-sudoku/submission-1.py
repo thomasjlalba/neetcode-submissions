282913e92be4c659dcbb620be5cfb617313d497f
Thomas Alba <88082961+thomasjlalba@users.noreply.github.com>
2026-07-14 21:18:25 +0800

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        hor = [set() for _ in range(9)]
+        ver = [set() for _ in range(9)]
+        square = [set() for _ in range(9)]
+
+        for row in range(len(board)):
+            for col, cell in enumerate(board[row]):
+                if cell == ".":
+                    continue
+                squarePosn = (col // 3) + (row // 3) * 3
+                if cell in hor[row] or cell in ver[col] or cell in square[squarePosn]:
+                    return False
+                # update them all
+                hor[row].add(cell)
+                ver[col].add(cell)
+                square[squarePosn].add(cell)
+        return True
