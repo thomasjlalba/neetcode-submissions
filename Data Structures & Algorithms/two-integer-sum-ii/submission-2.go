c514e35b2446fc524daad9fb83b2da2d3b0d16db
Thomas Alba <88082961+thomasjlalba@users.noreply.github.com>
2026-08-03 21:26:37 +0800

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.go
@@ -0,0 +1,11 @@
+func twoSum(numbers []int, target int) []int {
+    lp, rp := 0, len(numbers) - 1
+    for ans := numbers[lp] + numbers[rp]; ans != target; ans = numbers[lp] + numbers[rp] {
+        if ans < target {
+            lp++
+        } else {
+            rp--
+        }
+    }
+    return []int{lp + 1, rp + 1}
+}
